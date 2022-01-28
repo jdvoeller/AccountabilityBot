7f21540851501914422a03a97b9495374c43cabb
jdvoeller <42427496+jdvoeller@users.noreply.github.com>
2022-01-28 09:25:00 -0700

    got commands working in my server

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
+const USERS = {
+	jordan: '197921111973953536',
+}
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
 	console.log(client);
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'test') {
+		console.log(interaction);
+		await interaction.reply('harri is a piece of shit');
+	}
 });
 
 client.login(token);
